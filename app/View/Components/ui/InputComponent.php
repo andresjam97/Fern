dafd61cb5fb44b2c3858e2df2806547ad4acfefa
andresjam97 <castroruizivanandres@gmail.com>
2022-10-06 19:59:46 -0500

    cambio por componentes

---
+++ app/View/Components/ui/InputComponent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components\ui;
+
+use Illuminate\View\Component;
+
+class InputComponent extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public $label;
+    
+    public function __construct($label)
+    {
+        $this->label = $label;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.ui.input-component');
+    }
+}
--- resources/views/Clientes/clientes-index.blade.php
+++ resources/views/Clientes/clientes-index.blade.php
@@ -5,174 +5,70 @@
         </h2>
     </x-slot>
 
-    @if (\Session::has('success'))
-    <div class="bg-green-100 rounded-lg py-5 px-6 mb-4 text-base text-green-700 mb-3" role="alert">
-        <ul>
-            <li>{!! \Session::get('success') !!}</li>
-        </ul>
-    </div>
-@endif
-    @if ($errors->any())
-        @foreach ($errors->all() as $error)
-        <div class="bg-red-100 rounded-lg py-5 px-6 mb-4 text-base text-red-700 mb-3" role="alert">
-           {{$error}}
-          </div>
-        @endforeach
-    @endif
+
+
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    @if (\Session::has('success'))
+                        <div class="bg-green-100 rounded-lg py-5 px-6 mb-4 text-base text-green-700 mb-3" role="alert">
+                            <ul>
+                                <li>{!! \Session::get('success') !!}</li>
+                            </ul>
+                        </div>
+                    @endif
+                    @if ($errors->any())
+                        @foreach ($errors->all() as $error)
+                            <div class="bg-red-100 rounded-lg py-5 px-6 mb-4 text-base text-red-700 mb-3" role="alert">
+                                {{ $error }}
+                            </div>
+                        @endforeach
+                    @endif
                     <h1>Nuevo Cliente</h1><br>
-                        <form action="{{route('send-cliente')}}" method="POST">
+                    <form action="{{ route('send-cliente') }}" method="POST" autocomplete="off">
                         @csrf
-                          <div class="form-group mb-6">
-                            <label for="tipoCliente">Tipo Cliente</label>
-                            <select class="form-select appearance-none
-                            block
-                            w-full
-                            px-3
-                            py-1.5
-                            text-base
-                            font-normal
-                            text-gray-700
-                            bg-white bg-clip-padding bg-no-repeat
-                            border border-solid border-gray-300
-                            rounded
-                            transition
-                            ease-in-out
-                            m-0
-                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example" id="tipoCliente" name="tipoCliente" placeholder="Tipo">
-                              <option selected>Selecciona una opcion</option>
-                              <option value="1">Formulador</option>
-                              <option value="2">Distribuidor</option>
-                          </select>
-                          </div>
-                          <div class="form-group mb-6">
-                            <input type="text" class="form-control block
-                              w-full
-                              px-3
-                              py-1.5
-                              text-base
-                              font-normal
-                              text-gray-700
-                              bg-white bg-clip-padding
-                              border border-solid border-gray-300
-                              rounded
-                              transition
-                              ease-in-out
-                              m-0
-                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="NIT" name="NIT" value="{{old('NIT')}}"
-                              placeholder="NIT">
-                          </div>
-                          <div class="form-group mb-6">
-                            <input type="text" class="form-control block
-                              w-full
-                              px-3
-                              py-1.5
-                              text-base
-                              font-normal
-                              text-gray-700
-                              bg-white bg-clip-padding
-                              border border-solid border-gray-300
-                              rounded
-                              transition
-                              ease-in-out
-                              m-0
-                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="nombre" name="nombre" value="{{old('nombre')}}"
-                              placeholder="Nombre">
-                          </div>
-                          <div class="form-group mb-6">
-                            <input type="email" class="form-control block
-                              w-full
-                              px-3
-                              py-1.5
-                              text-base
-                              font-normal
-                              text-gray-700
-                              bg-white bg-clip-padding
-                              border border-solid border-gray-300
-                              rounded
-                              transition
-                              ease-in-out
-                              m-0
-                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="mail" name="mail" value="{{old('mail')}}"
-                              placeholder="E-mail">
-                          </div>
-                          <div class="form-group mb-6">
-                            <label for="tipoCliente">Ciudad</label>
-                            <select class="form-select appearance-none
-                            block
-                            w-full
-                            px-3
-                            py-1.5
-                            text-base
-                            font-normal
-                            text-gray-700
-                            bg-white bg-clip-padding bg-no-repeat
-                            border border-solid border-gray-300
-                            rounded
-                            transition
-                            ease-in-out
-                            m-0
-                            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example" id="ciudad" name="ciudad" placeholder="Tipo">
-                              <option selected>Selecciona una opcion</option>
-                              <option value="1">Ciudad1</option>
-                              <option value="2">ciudad2</option>
-                          </select>
-                          </div>
-                          <div class="form-group mb-6">
-                            <input type="text" class="form-control block
-                              w-full
-                              px-3
-                              py-1.5
-                              text-base
-                              font-normal
-                              text-gray-700
-                              bg-white bg-clip-padding
-                              border border-solid border-gray-300
-                              rounded
-                              transition
-                              ease-in-out
-                              m-0
-                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="direccion" name="direccion" value="{{old('direccion')}}"
-                              placeholder="Direccion">
-                          </div>
-                          <div class="form-group mb-6">
-                            <input type="phone" class="form-control block
-                              w-full
-                              px-3
-                              py-1.5
-                              text-base
-                              font-normal
-                              text-gray-700
-                              bg-white bg-clip-padding
-                              border border-solid border-gray-300
-                              rounded
-                              transition
-                              ease-in-out
-                              m-0
-                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="tel1" name="tel1" value="{{old('tel1')}}"
-                              placeholder="Telefono 1">
-                          </div>
-                          <div class="form-group mb-6">
-                            <input type="phone" class="form-control block
-                              w-full
-                              px-3
-                              py-1.5
-                              text-base
-                              font-normal
-                              text-gray-700
-                              bg-white bg-clip-padding
-                              border border-solid border-gray-300
-                              rounded
-                              transition
-                              ease-in-out
-                              m-0
-                              focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="tel2" name="tel2" value="{{old('tel2')}}"
-                              placeholder="Telefono 2">
-                          </div>
-                          <button type="submit" class="
+                        <div class="form-group mb-6">
+                            <x-ui.select-component label="Tipo Cliente" name="tipoCliente" id="tipoCliente">
+                                <option selected>Selecciona una opcion</option>
+                                <option value="1">Formulador</option>
+                                <option value="2">Distribuidor</option>
+                            </x-ui.select-component>
+                        </div>
+                        <div class="form-group mb-6">
+                            <x-ui.input-component label="NIT" type="text" name="NIT" id="NIT"
+                                placeholder="Nit" />
+                        </div>
+                        <div class="form-group mb-6">
+                            <x-ui.input-component label="Nombre" type="text" name="nombre" id="nombre"
+                                placeholder="Nombre" />
+                        </div>
+                        <div class="form-group mb-6">
+                            <x-ui.input-component label="Email" type="email" name="mail" id="mail"
+                                placeholder="Email" />
+                        </div>
+                        <div class="form-group mb-6">
+                            <x-ui.select-component label="Ciudad" name="ciudad" id="ciudad">
+                                <option selected>Selecciona una opcion</option>
+                                <option value="1">Ciudad1</option>
+                                <option value="2">ciudad2</option>
+                            </x-ui.select-component>
+                        </div>
+                        <div class="form-group mb-6">
+                            <x-ui.input-component label="Direccion" type="text" name="direccion" id="direccion"
+                                placeholder="Direccion" />
+                        </div>
+                        <div class="form-group mb-6">
+                            <x-ui.input-component label="Telefono - 1" type="phone" name="tel1" id="tel1"
+                                placeholder="Telefono 1" />
+                        </div>
+                        <div class="form-group mb-6">
+                            <x-ui.input-component label="Telefono - 2" type="phone" name="tel2" id="tel2"
+                                placeholder="Telefono 2" />
+                        </div>
+                        <button type="submit"
+                            class="
                             w-full
                             px-6
                             py-2.5
@@ -189,8 +85,8 @@
                             active:bg-blue-800 active:shadow-lg
                             transition
                             duration-150
-                            ease-in-out">Send</button>
-                        </form>
+                            ease-in-out">Enviar</button>
+                    </form>
                 </div>
             </div>
         </div>
