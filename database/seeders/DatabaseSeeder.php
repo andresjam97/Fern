942c1d5f586c78ca8f34dc70b4f7f113d8800e80
Ivan Andres Castro Ruiz <19806500+andresjam97@users.noreply.github.com>
2022-11-21 11:55:43 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CreateAdminUserSeeder;
+use Database\Seeders\PermissionTableSeeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            CreateAdminUserSeeder::class,
+            PermissionTableSeeder::class,
+        ]);
     }
 }
